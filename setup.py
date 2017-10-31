ec9794563a5264973354aea2068ef72da259feca
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-31 14:52:56 +1300

    setup init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpikesData',
+    version='0.1a0',
+    packages=['spikesdata'],
+    url='https://github.com/akshaybabloo/SpikesData',
+    license='MIT',
+    author='Akshay Raj Gollahalli',
+    author_email='akshay@gollahalli.com',
+    description='Temporal data parser.'
+)
